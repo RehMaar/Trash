f28ae33ff7564bd6bee45928f647a96c50812e4f
Maria Kuklina <mkdirwtf@gmail.com>
2017-03-12 16:23:37 +0300

    Add third task files; need UML.

---
+++ SoftwareTesting/lab1/thirdTask/src/test/java/galaxy/TestGalaxy.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertations.*;
+
+import galaxy.*;
+
+class TestGalaxyModule {
+
+	@BeforeEach
+	void initModule() {
+	}
+
+}
