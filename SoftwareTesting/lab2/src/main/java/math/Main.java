f06ee1ba96893f2036566c0c6ed37a94d26d45cb
Maria Kuklina <mkdirwtf@gmail.com>
2017-04-07 09:09:09 +0300

    Revert "added logging for all functions"
    This reverts commit 50dad9bb2af9f122e15cbca75c1fab06520e6079.

--- SoftwareTesting/lab2/src/main/java/math/Main.java
+++ SoftwareTesting/lab2/src/main/java/math/Main.java
@@ -4,10 +4,8 @@ package math;
  * Created by anki0216 on 03.04.2017.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SystemSolver s = new SystemSolver();
-        for (Double i = -0.1; i < 1.0; i += 0.1) {
-            System.out.println(i + " " + s.solve(i));
-        }
+        System.out.println(s.solve(-Math.PI));
     }
 }
