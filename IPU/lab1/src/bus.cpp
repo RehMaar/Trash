dc2f5066d62266018e3d8916409da3dbd68cf56a
Maria Kuklina <mkdirwtf@gmail.com>
2017-10-17 22:29:41 +0300

    IPU: lab1: fix bus

--- IPU/lab1/src/bus.cpp
+++ IPU/lab1/src/bus.cpp
@@ -1,40 +1,45 @@
+#include <stdio.h>
 #include "bus.hpp"
 
-using namespace timer;
-using namespace bus;
 using namespace std;
 
-pair<addr_t, slave_num>  bus::decode(mem_map addr) {
-    switch (addr) {
-        case TMR_TM1:
-            return make_pair(reg_map_addr::TMR_ADDR, slave_num::TM1);
-        case TVAL_TM1:
-            return make_pair(reg_map_addr::TMR_ADDR, slave_num::TM1);
-        case TCONF_TM1:
-            return make_pair(reg_map_addr::TMR_ADDR, slave_num::TM1);
-        case TMR_TM2:
-            return make_pair(reg_map_addr::TMR_ADDR, slave_num::TM2);
-        case TVAL_TM2:
-            return make_pair(reg_map_addr::TMR_ADDR, slave_num::TM2);
-        case TCONF_TM2:
-            return make_pair(reg_map_addr::TMR_ADDR, slave_num::TM2);
-        case OCCONF_OC:
-            return make_pair(oc::reg_map_addr::OCCONF_ADDR, slave_num::OC);
-        case OCR_OC:
-            return make_pair(oc::reg_map_addr::OCR_ADDR, slave_num::OC);
-    }
-    return make_pair((addr_t)0, (slave_num)0);
+bus::bus(sc_module_name nm) : sc_module(nm) {
+    SC_METHOD(channel_switch)
+        sensitive << rd_i << wr_i;
 }
 
-uint32_t bus::channel_switch() {
+pair<addr_t, bus::slave_addr>  bus::decode(mem_map addr) {
+    switch (addr) {     
+        case TMR_TM1:
+            return make_pair(timer::timer_reg_map::TMR_ADDR, bus::slave_addr::TM1);
+        case TVAL_TM1:
+            return make_pair(timer::timer_reg_map::TVAL_ADDR, bus::slave_addr::TM1);
+        case TCONF_TM1:
+            return make_pair(timer::timer_reg_map::TCONF_ADDR, bus::slave_addr::TM1);
+        case TMR_TM2:
+            return make_pair(timer::timer_reg_map::TMR_ADDR, bus::slave_addr::TM2);
+        case TVAL_TM2:
+            return make_pair(timer::timer_reg_map::TVAL_ADDR, bus::slave_addr::TM2);
+        case TCONF_TM2:
+            return make_pair(timer::timer_reg_map::TCONF_ADDR, bus::slave_addr::TM2);
+        case OCCONF_OC:
+            return make_pair(oc::oc_reg_map::OCCONF_ADDR, bus::slave_addr::OC);
+        case OCR_OC:
+            return make_pair(oc::oc_reg_map::OCR_ADDR, bus::slave_addr::OC);
+    }
+    return make_pair((addr_t)0, (bus::slave_addr)0);
+}
+
+void bus::channel_switch() {
     /* Decode address. */
-    pair<addr_t, slave_num> p = decode((mem_map)addr_i.read());
+    pair<addr_t, bus::slave_addr> p = bus::decode((mem_map)addr_i.read());
+    bus::slave_signals ss = channels[p.second];
 
-    slave_signals ss = channels[p.second]
-
-    ss.addr_o.write(p.first); 
-    data_o.write(ss.data_i.read());
-    ss.rd_o.write(rd_i.read());
-    ss.wr_o.write(wr_i.read();
-
+    /* Set local addres. */
+    (*ss.addr_o).write(p.first);
+    /* Write data to output. WTF? */
+    data_o.write((*ss.data_i).read());
+    /* Send signals to the slave.*/
+    (*ss.rd_o).write(rd_i.read());
+    (*ss.wr_o).write(wr_i.read());
 }
--- IPU/lab1/src/bus.hpp
+++ IPU/lab1/src/bus.hpp
@@ -4,66 +4,65 @@
 #include <systemc.h>
 #include "defines.hpp"
 #include "timer.hpp"
+#include "output_compare.hpp"
+#include "mem_map.hpp"
 
 
-SC_METHOD (bus_matrix) {
+SC_MODULE (bus) {
     sc_in_clk          clk_i;
     sc_in<bool>        rst_i;
 
     /* From Master. */
-    sc_in<sc_uint<32>> addr_i;
-    sc_in<sc_uint<32>> data_i;
-    sc_in<bool>        rd_i;
-    sc_in<bool>        wr_i;
+    sc_in<uint32_t> addr_i;
+    sc_in<bool>     rd_i;
+    sc_in<bool>     wr_i;
     /* To Master */
-    sc_out<sc_uint<32>> data_o;
+    sc_out<uint32_t> data_o;
 
     /* To slaves. */
     /* Timer 1. */
-    sc_out<sc_uint<32>> tm1_addr_o;
-    sc_out<sc_uint<32>> tm1_data_o;
-    sc_out<bool>        tm1_rd_o;
-    sc_out<bool>        tm1_wr_o;
-	/* Timer 2. */
-    sc_in<sc_uint<32>> tm2_data_i;
+    sc_in<uint32_t>  tm1_data_i;
+    sc_out<uint32_t> tm1_addr_o;
+    sc_out<bool>     tm1_rd_o;
+    sc_out<bool>     tm1_wr_o;
+    /* Timer 2. */
+    sc_in<uint32_t>  tm2_data_i;
+    sc_out<uint32_t> tm2_addr_o;
+    sc_out<bool>      tm2_rd_o;
+    sc_out<bool>      tm2_wr_o;
 
-    sc_out<sc_uint<32>> tm2_addr_o;
-    sc_out<bool>        tm2_rd_o;
-    sc_out<bool>        tm2_wr_o;
+    /* Output Compare. */
+    sc_in<uint32_t>  oc_data_i;
+    sc_out<uint32_t> oc_addr_o;
+    sc_out<bool>     oc_rd_o;
+    sc_out<bool>     oc_wr_o;
 
-	/* Output Compare. */
-    sc_in<sc_uint<32>> oc_data_i;
+    struct slave_signals {
+        sc_in <uint32_t> *data_i;
+        sc_out<uint32_t> *addr_o;
+        sc_out<bool>     *rd_o;
+        sc_out<bool>     *wr_o;
+    };
 
-    sc_out<sc_uint<32>> oc_addr_o;
-    sc_out<bool>        oc_rd_o;
-    sc_out<bool>        oc_wr_o;
+    enum slave_addr {
+        TM1,
+        TM2,
+        OC,
+        SLAVE_NUM
+    };
 
-	struct slave_signals {
-		sc_out<sc_uint<32>> data_i;
-		sc_out<sc_uint<32>> addr_o;
-		sc_out<bool> 	    rd_o;
-		sc_out<bool>		wr_o;
-	};
+    slave_signals channels[SLAVE_NUM] = {
+        { &tm1_data_i, &tm1_addr_o, &tm1_rd_o, &tm1_wr_o},
+        { &tm2_data_i, &tm2_addr_o, &tm2_rd_o, &tm2_wr_o},
+        { &oc_data_i, &oc_addr_o, &oc_rd_o, &oc_wr_o}
+    };
 
-private:
+    std::pair<addr_t, slave_addr> decode(mem_map addr);
 
-	enum slave_addr {
-		TM1,
-		TM2,
-		OC,
-		SLAVE_NUM
-	};
-
-	slave_signals channels[SLAVE_NUM] = {
-		{ &tm1_data_i, &tm1_addr_o, &tm1_rd_o, &tm1_wr_o},
-		{ &tm2_data_i, &tm2_addr_o, &tm2_rd_o, &tm2_wr_o},
-		{ &oc_data_i, &oc_addr_o, &oc_rd_o, &oc_wr_o}
-	}
-
-	slave_num decode(sc_uint<32> addr);
+    void channel_switch();
 
 public:
-	void swtich_channels();
+    SC_CTOR(bus);
 };
 
 #endif // bus_hpp_INCLUDED
